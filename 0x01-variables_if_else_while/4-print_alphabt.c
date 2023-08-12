1c4ac6908c21c034914ea2682a3de2ff62e4f5e5
faahvor <tinafavour2018@gmail.com>
2023-08-11 18:25:32 -0700

    for

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
 
 /**
- * 
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'q')
+		{
+			continue;
+		}
+		else if (alpha == 'e')
+		{
+			continue;
+		}
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
