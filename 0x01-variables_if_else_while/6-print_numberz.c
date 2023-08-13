af1e5e083a8e9a985829aa8ebbf049e96d87572d
faahvor <tinafavour2018@gmail.com>
2023-08-12 17:23:49 -0700

    print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,15 @@
 int main(void)
 {
 	char num[] = "0123456789";
+	int i;
 
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
+	for (i =48; i < 58 ; i++)	
 	{
 		putchar(num[i]);
+		putchar(1);
 	}
 
 	putchar('\n');
-  return 0;
+	return (0);
 }
