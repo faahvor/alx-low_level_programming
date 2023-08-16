a687233eb9b826606ed2342cb9658a6fbafe5b76
faahvor <tinafavour2018@gmail.com>
2023-08-16 03:09:24 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+ /**
+  * print_alphabet_10 - making lowercase alphabet
+  *
+  * decription: printing lowercase alphabets
+  *
+  * Return: Always 0 (Success)
+  */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
