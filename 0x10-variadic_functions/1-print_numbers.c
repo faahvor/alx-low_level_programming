09b2cc1080fdaa5f270b63be8bb24ef9279f8505
faahvor <tinafavour2018@gmail.com>
2023-09-14 16:19:43 -0700

    variad

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers available
+ * @n: size of itereation
+ * @separator: seperator ,
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list print;
+
+	va_start(print, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(print, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(print);
+}
